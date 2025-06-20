6d3e68c6b2c65450e142d4c6b3479ae734568cec
sam <sjwyss@outlook.com>
2025-06-19 21:32:54 -0400

    refactored em_engine to emengine

--- src/core/em_engine.h
+++
@@ -1,9 +0,0 @@
-#ifndef CORE_EM_ENGINE_H
-#define CORE_EM_ENGINE_H
-
-class EM_Engine {
-public:
-  ~EM_Engine();
-};
-
-#endif // CORE_EM_ENGINE_H
---
+++ src/core/emengine.h
@@ -0,0 +1,9 @@
+#ifndef CORE_EMENGINE_H
+#define CORE_EMENGINE_H
+
+class EMEngine {
+public:
+  ~EMEngine();
+};
+
+#endif // CORE_EMENGINE_H
