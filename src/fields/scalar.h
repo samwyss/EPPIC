c16d1c2c3518951e7c7c36781474cc6403df29ec
sam <sjwyss@outlook.com>
2025-06-23 11:15:41 -0400

    updated Scalar3 to use mdspan

--- src/fields/scalar.h
+++ src/fields/scalar.h
@@ -1,7 +1,6 @@
 #ifndef FIELDS_SCALAR_H
 #define FIELDS_SCALAR_H
 
-#include <cstddef>
 #include <mdspan/mdspan.hpp>
 #include <memory>
 
@@ -10,19 +9,19 @@
 template <typename T> struct Scalar3 {
   /*!
    * Scalar3 constructor
-   * @param nx number of elements in the x-direction
-   * @param ny number of elements in the y-direction
-   * @param nz number of elements in the z-direction
-   * @param val initial value
+   * @param dims vector field dimensions
+   * @param val initial field value
    */
-  Scalar3(const size_t nx, const size_t ny, const size_t nz, T val)
-      : cell_counts(Triplet{nx, ny, nz}), row_off(nz), plane_off(ny * nz),
-        num_cells(nx * ny * nz) {
-    // create data container
-    data = std::make_unique<T[]>(num_cells);
+  Scalar3(const Coord3<size_t> &dims, const T val) {
+    const size_t nelems = dims.x * dims.y * dims.z;
 
-    // initialize data
-    *this = val;
+    data_arr = std::make_unique<T[]>(nelems);
+
+    data = Kokkos::mdspan(data_arr.get(), dims);
+
+    for (auto elem : data) {
+      elem = val;
+    }
   }
 
   /*!
@@ -30,56 +29,12 @@ template <typename T> struct Scalar3 {
    */
   ~Scalar3() = default;
 
-  /*!
-   * Scalar3 overloaded () operator for read-write access
-   * @param i index into x-direction elements
-   * @param j index into y-direction elements
-   * @param k index into z-direction elements
-   * @return mutable element at desired index
-   */
-  T &operator()(const size_t i, const size_t j, const size_t k) {
-    return data.get()[k + row_off * j + plane_off * i];
-  }
-
-  /*!
-   * Scalar3 overloaded () operator for read access
-   * @param i index into x-direction elements
-   * @param j index into y-direction elements
-   * @param k index into z-direction elements
-   * @return element at desired index
-   */
-  T operator()(const size_t i, const size_t j, const size_t k) const {
-    return data.get()[k + row_off * j + plane_off * i];
-  };
+  /// data view
+  Kokkos::mdspan<double, Kokkos::dextents<size_t, 3>> data;
 
 private:
-  /*!
-   * Scalar3 overloaded assignment operator to initialize field
-   * @param val initial value
-   * @return reference to existing Scalar3
-   */
-  Scalar3 &operator=(const T &val) {
-    for (size_t i = 0; i < num_cells; ++i) {
-      data.get()[i] = val;
-    }
-
-    return *this;
-  }
-
-  /// 1D array in row-major format containing field data
-  std::unique_ptr<T[]> data;
-
-  /// number of cells in each direction
-  const Triplet<size_t> cell_counts;
-
-  /// row offset into data
-  const size_t row_off;
-
-  /// column offset into data
-  const size_t plane_off;
-
-  /// number of cells in bounding box volume
-  const size_t num_cells;
+  // data container
+  std::unique_ptr<T[]> data_arr;
 };
 
 #endif // FIELDS_SCALAR_H
