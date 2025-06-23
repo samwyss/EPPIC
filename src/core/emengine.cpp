88fcbb87ec43942f729d977df5ec4f620b008cc9
sam <sjwyss@outlook.com>
2025-06-23 15:04:16 -0400

    began setting up EMEngine

--- src/core/emengine.cpp
+++ src/core/emengine.cpp
@@ -1,1 +1,12 @@
 #include "emengine.h"
+
+EMEngine::EMEngine(const Config &config)
+    : e(Vector3<double>({1, 1, 1}, 0.0)), h(Vector3<double>({1, 1, 1}, 0.0)) {}
+
+std::expected<EMEngine, std::string> EMEngine::create(const Config &config) {
+  try {
+    return std::expected<EMEngine, std::string>{std::in_place, config};
+  } catch (const std::runtime_error &err) {
+    return std::unexpected(err.what());
+  }
+}
--- src/core/emengine.h
+++ src/core/emengine.h
@@ -1,9 +1,42 @@
 #ifndef CORE_EMENGINE_H
 #define CORE_EMENGINE_H
 
+#include <expected>
+#include <stdexcept>
+#include <string>
+
+#include "config.h"
+#include "vector.h"
+
 class EMEngine {
 public:
-  ~EMEngine();
+  /*!
+   * EMEngine static factory method
+   * @param config configuration structure
+   * @return
+   */
+  static std::expected<EMEngine, std::string> create(const Config &config);
+
+  /*!
+   * EMEngine destructor
+   */
+  ~EMEngine() = default;
+
+private:
+  /*!
+   * EMEngine constructor
+   * @param config configuration structure
+   */
+  explicit EMEngine(const Config &config);
+
+  /// (V/m) electric field vector
+  Vector3<double> e;
+
+  /// (A/m) magnetic field vector
+  Vector3<double> h;
+
+  /// (s) elapsed time
+  double time = 0.0;
 };
 
 #endif // CORE_EMENGINE_H
