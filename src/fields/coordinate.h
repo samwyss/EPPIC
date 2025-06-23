879bf9e2ba5fbee5e5b9469962b585257ae21765
sam <sjwyss@outlook.com>
2025-06-23 09:45:27 -0400

    refactored to use Coordinate based names

--- src/fields/coordinate.h
+++ src/fields/coordinate.h
@@ -1,23 +1,19 @@
 #ifndef FIELDS_COORDINATE_H
 #define FIELDS_COORDINATE_H
 
-template <typename T> struct Triplet {
-  /// x-component
+template <typename T> struct Coord3 {
   const T x;
-
-  /// y-component
   const T y;
-
-  /// z-component
   const T z;
 };
 
-template <typename T> struct Pair {
-  /// x-component
+template <typename T> struct Coord2 {
   const T x;
-
-  /// y-component
   const T y;
+};
+
+template <typename T> struct Coord1 {
+  const T x;
 };
 
 #endif // FIELDS_COORDINATE_H
