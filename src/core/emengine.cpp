29cc397cdb7c97b0cf91f8677879a33205f20968
sam <sjwyss@outlook.com>
2025-06-24 12:38:15 -0400

    added first pass at FDTDGeometry

--- src/core/emengine.cpp
+++ src/core/emengine.cpp
@@ -1,11 +1,57 @@
 #include "emengine.h"
 
-EMEngine::EMEngine(const Config &config)
+FDTDGeometry::FDTDGeometry(const Config &config)
+    : x_len(config.x_len), y_len(config.y_len), z_len(config.z_len),
+      ep_r(config.ep_r), mu_r(config.mu_r), ep(ep_r * VAC_PERMITTIVITY),
+      mu(mu_r * VAC_PERMEABILITY), sigma(config.sigma) {
+
+  // (m) minimum spatial step based on maximum frequency
+  const double ds_min_wavelength =
+      (VAC_SPEED_OF_LIGHT / (config.max_frequency * sqrt(ep_r * mu_r))) /
+      static_cast<double>(config.num_vox_min_wavelength);
+
+  // (m) minimum spatial step based on minimum feature size
+  const double ds_min_feature_size =
+      std::min({x_len, y_len, z_len}) /
+      static_cast<double>(config.num_vox_min_feature);
+
+  // (m) minimum required spatial step
+  const double ds = std::min({ds_min_wavelength, ds_min_feature_size});
+
+  // number of voxels in each direction snapped to ds
+  nvx = ceil(x_len / ds);
+  nvy = ceil(y_len / ds);
+  nvz = floor(z_len / ds);
+
+  // (m) final spatial steps
+  dx = x_len / static_cast<double>(nvx);
+  dy = y_len / static_cast<double>(nvy);
+  dz = z_len / static_cast<double>(nvz);
+
+  // (m^-1) inverse spatial steps
+  dx_inv = 1.0 / dx;
+  dy_inv = 1.0 / dy;
+  dz_inv = 1.0 / dz;
+
+  // todo diagnostics
+}
+
+std::expected<FDTDGeometry, std::string>
+FDTDGeometry::create(const Config &config) {
+  try {
+    return FDTDGeometry(config);
+  } catch (const std::runtime_error &err) {
+    return std::unexpected(err.what());
+  }
+}
+
+FDTDEngine::FDTDEngine(const Config &config)
     : e(Vector3<double>({1, 1, 1}, 0.0)), h(Vector3<double>({1, 1, 1}, 0.0)) {}
 
-std::expected<EMEngine, std::string> EMEngine::create(const Config &config) {
+std::expected<FDTDEngine, std::string>
+FDTDEngine::create(const Config &config) {
   try {
-    return EMEngine(config);
+    return FDTDEngine(config);
   } catch (const std::runtime_error &err) {
     return std::unexpected(err.what());
   }
--- src/core/emengine.h
+++ src/core/emengine.h
@@ -1,28 +1,94 @@
 #ifndef CORE_EMENGINE_H
 #define CORE_EMENGINE_H
 
+#include <algorithm>
 #include <expected>
 #include <stdexcept>
 #include <string>
 
 #include "config.h"
+#include "physical.h"
 #include "vector.h"
 
-class EMEngine {
+class FDTDGeometry {
 public:
   /*!
-   * EMEngine static factory method
-   * @param config configuration structure
-   * @return
+   * FDTDGeometry static factory method
+   * @param config configuration object
+   * @return FDTDGeometry
    */
-  static std::expected<EMEngine, std::string> create(const Config &config);
+  static std::expected<FDTDGeometry, std::string> create(const Config &config);
+
+  /// (m) size of bounding box in the x-direction
+  const double x_len;
+
+  /// (m) size of bounding box in the y-direction
+  const double y_len;
+
+  /// (m) size of bounding box in the z-direction
+  const double z_len;
+
+  /// relative diagonally isotropic permittivity of material inside bounding box
+  const double ep_r;
+
+  /// relative diagonally isotropic permeability of material inside bounding box
+  const double mu_r;
+
+  /// (F/m) diagonally isotropic permittivity of material inside bounding box
+  const double ep;
+
+  /// (H/m) diagonally isotropic permeability of material inside bounding box
+  const double mu;
+
+  /// (S/m) diagonally isotropic conductivity of material inside bounding box
+  const double sigma;
+
+  /// (m) spatial increment in x-direction
+  double dx;
+
+  /// (m) spatial increment in y-direction
+  double dy;
+
+  /// (m) spatial increment in z-direction
+  double dz;
+
+  /// (m) inverse spatial increment in x-direction
+  double dx_inv;
+
+  /// (m) inverse spatial increment in y-direction
+  double dy_inv;
+
+  /// (m) inverse spatial increment in z-direction
+  double dz_inv;
+
+  /// number of voxels in x-direction
+  size_t nvx;
+
+  /// number of voxels in y-direction
+  size_t nvy;
+
+  /// number of voxels in z-direction
+  size_t nvz;
+
+private:
+  FDTDGeometry(const Config &config);
+};
+
+class FDTDEngine {
+public:
+  /*!
+   * FDTDEngine static factory method
+   * @param config configuration object
+   * @return FDTDEngine
+   */
+  static std::expected<FDTDEngine, std::string> create(const Config &config);
 
 private:
   /*!
-   * EMEngine constructor
-   * @param config configuration structure
+   * FDTDEngine constructor
+   * @param config configuration object
    */
-  explicit EMEngine(const Config &config);
+  explicit FDTDEngine(const Config &config);
 
   /// (V/m) electric field vector
   Vector3<double> e;
