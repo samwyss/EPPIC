334a062d4b72e94dbd8e636ff0ca9406b2e98a3d
sam <sjwyss@outlook.com>
2025-06-24 12:23:54 -0400

    added vacuum speed of light

--- src/constants/physical.h
+++ src/constants/physical.h
@@ -1,15 +1,17 @@
 #ifndef CONSTANTS_PHYSICAL_HPP
 #define CONSTANTS_PHYSICAL_HPP
 
-# include <numbers>
+#include <numbers>
 
-using namespace std::numbers;
-
-/// (F / m) vacuum permittivity https://en.wikipedia.org/wiki/Vacuum_permittivity
+/// (F/m) vacuum permittivity https://en.wikipedia.org/wiki/Vacuum_permittivity
 constexpr double VAC_PERMITTIVITY = 8.8541878188e-12;
 
-/// (H / m) vacuum permeability https://en.wikipedia.org/wiki/Vacuum_permeability
+/// (H/m) vacuum permeability https://en.wikipedia.org/wiki/Vacuum_permeability
 constexpr double VAC_PERMEABILITY = 4.0 * std::numbers::pi * 1e-7;
+
+/// (m/s) vacuum speed of light
+constexpr double VAC_SPEED_OF_LIGHT =
+    1.0 / (VAC_PERMITTIVITY * VAC_PERMEABILITY);
 
 /// (C) electron charge https://en.wikipedia.org/wiki/Elementary_charge
 constexpr double ELEC_CHARGE = 1.602176634e-19;
@@ -20,10 +22,10 @@ constexpr double AMU = 1.66053906892e-27;
 /// (kg) electron mass https://en.wikipedia.org/wiki/Electron_mass
 constexpr double ELEC_MASS = 9.1093837139e-31;
 
-/// (J / K) boltzmann constant https://en.wikipedia.org/wiki/Boltzmann_constant
+/// (J/K) boltzmann constant https://en.wikipedia.org/wiki/Boltzmann_constant
 constexpr double BOLTZMANN = 1.380649e-23;
 
 /// (K) electron volt temperature
 constexpr double EV_TEMP = ELEC_CHARGE / BOLTZMANN;
 
-#endif //CONSTANTS_PHYSICAL_HPP
+#endif // CONSTANTS_PHYSICAL_HPP
