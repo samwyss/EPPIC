a4f765865393e80557a0dbf7901d0645705a26c7
sam <sjwyss@outlook.com>
2025-06-24 21:05:46 -0400

    added basic skeleton similar to emengine

--- src/core/world.cpp
+++ src/core/world.cpp
@@ -1,6 +1,7 @@
 #include "world.h"
 
-World::World() { throw std::runtime_error("World::World()"); }
+World::World()
+    : config(Config()), emengine(FDTDEngine::create(config).value()) {}
 
 std::expected<World, std::string> World::create() {
   try {
@@ -10,6 +11,24 @@ std::expected<World, std::string> World::create() {
   }
 }
 
-std::expected<void, std::string> World::adv_by() { return {}; }
+std::expected<void, std::string> World::advance_to(const double end_t) {
+  if (end_t > time) {
+    // (s) time difference between current state and end time
+    const double adv_t = end_t - time;
 
-std::expected<void, std::string> World::adv_to() { return {}; }
+    advance_by(adv_t).value();
+  }
+  return {};
+}
+
+std::expected<void, std::string> World::advance_by(const double adv_t) {
+
+  // get timesteps from sub solvers
+
+  // loop
+
+  // todo remove me
+  emengine.advance_by(adv_t).value();
+
+  return {};
+}
--- src/core/world.h
+++ src/core/world.h
@@ -5,16 +5,50 @@
 #include <stdexcept>
 #include <string>
 
+#include "config.h"
+#include "emengine.h"
+
+/*!
+ * world object
+ */
 class World {
 public:
+  /*!
+   * World static factory method
+   * @return void
+   */
   static std::expected<World, std::string> create();
 
-  static std::expected<void, std::string> adv_by();
+  /*!
+   * advances internal state to an end time
+   *
+   * will do nothing in the event that end_t <= time
+   * @param end_t (s) end time
+   * @return void
+   */
+  std::expected<void, std::string> advance_to(double end_t);
 
-  static std::expected<void, std::string> adv_to();
+  /*!
+   * advances internal state by a given time period
+   * @param adv_t (s) time period to advance by
+   * @return void
+   */
+  std::expected<void, std::string> advance_by(double adv_t);
 
 private:
+  /*!
+   * World constructor
+   */
   World();
+
+  /// model configuration
+  const Config config;
+
+  /// electromagnetic engine
+  FDTDEngine emengine;
+
+  /// (s) elapsed time
+  double time = 0.0;
 };
 
 #endif // CORE_WORLD_H
