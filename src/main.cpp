1eebc486a330ab54bb99afba57d839426791814c
sam <sjwyss@outlook.com>
2025-06-26 08:25:49 -0400

    removed logger in favor of compile time logging level selection

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <mdspan/mdspan.hpp>
 #include <spdlog/spdlog.h>
 
+#include <filesystem>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/spdlog.h>
 #include <string>
 
-#include "logger.h"
 #include "scalar.h"
 #include "vector.h"
 #include "world.h"
@@ -25,17 +27,40 @@ int main(int argc, char **argv) {
   // configuration
   // parallelization
 
-  spdlog::set_level(spdlog::level::debug);
-
+  // todo get this from configuration
   const std::string fp_precision = "double";
 
-  auto _ = Logger::init();
-  auto logger = Logger::get();
+  // auto _ = Logger::init();
+  // auto logger = Logger::get();
+
+  // initial diagnostics
+  // logger->info("EPPIC run starting");
+
+  // setup spdlog
+  if (std::filesystem::is_directory("./logs/")) {
+    std::filesystem::remove_all("./logs/");
+  }
+  std::filesystem::create_directory("./logs/");
+  const auto logger = spdlog::basic_logger_mt("logger", "./logs/log.log");
+  logger->set_level(spdlog::level::trace);
+  spdlog::set_default_logger(logger);
 
   if (fp_precision == "double") {
     const auto config = Config<double>();
     auto world = World<double>::create(config).value();
     world.advance_to(config.end_time).value();
+    SPDLOG_CRITICAL("critical");
+    SPDLOG_ERROR("error");
+    SPDLOG_WARN("warn");
+    SPDLOG_INFO("info");
+    SPDLOG_DEBUG("debug");
+    SPDLOG_TRACE("trace");
+    SPDLOG_CRITICAL("critical");
+    SPDLOG_ERROR("error");
+    SPDLOG_WARN("warn");
+    SPDLOG_INFO("info");
+    SPDLOG_DEBUG("debug");
+    SPDLOG_TRACE("trace");
   } else if (fp_precision == "single") {
     const auto config = Config<float>();
     auto world = World<float>::create(config).value();
