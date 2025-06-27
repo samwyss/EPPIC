cb9fc915546c6946c730342e93aefeeb5e0c3720
sam <sjwyss@outlook.com>
2025-06-27 09:11:24 -0400

    added numeric constants header

---
+++ src/constants/numeric.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_NUMERIC_H
+#define CONSTANTS_NUMERIC_H
+
+#include <cmath>
+
+template <std::floating_point T>
+constexpr T ONE_OVER_TWO = static_cast<T>(1.0 / 2.0);
+
+#endif // CONSTANTS_NUMERIC_H
