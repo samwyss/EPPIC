c71f3f6f6434fd378c06c4b315f8b7b550649251
sam <sjwyss@outlook.com>
2025-07-10 11:33:10 -0400

    removed h5fpp as the way HDF5 manages type macros prevents constexpr evaluation

--- src/constants/type.h
+++ src/constants/type.h
@@ -9,9 +9,4 @@ using fpp = float;
 // ensure fpp is either double or float to use correct HDF5 and MPI type aliases
 static_assert(std::is_same_v<fpp, double> || std::is_same_v<fpp, float>);
 
-/// HDF5 floating point precision
-using h5fpp =
-    std::conditional_t<std::is_same_v<fpp, double>, decltype(H5T_NATIVE_DOUBLE),
-                       decltype(H5T_NATIVE_FLOAT)>;
-
 #endif // CONSTANTS_TYPE_H
