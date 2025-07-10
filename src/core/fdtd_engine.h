2f33af0086c9cfc14db4d36f68615ea850d1227f
sam <sjwyss@outlook.com>
2025-07-09 21:40:38 -0400

    changed to use floating point type alias instead of templates

--- src/core/fdtd_engine.h
+++ src/core/fdtd_engine.h
@@ -14,13 +14,13 @@
 #include "coordinate.h"
 #include "numeric.h"
 #include "physical.h"
+#include "type.h"
 #include "vector.h"
 
 /*!
  * FDTD engine object
- * @tparam T floating point precision
  */
-template <std::floating_point T> class FDTDEngine {
+class FDTDEngine {
 public:
   /*!
    * FDTDEngine static factory method
@@ -28,7 +28,7 @@ public:
    * @return FDTDEngine
    */
   [[nodiscard]] static std::expected<FDTDEngine, std::string>
-  create(const Config<T> &config);
+  create(const Config &config);
 
   /*!
    * calculates the number of steps required to advance engine state by some
@@ -36,14 +36,14 @@ public:
    * @param adv_t (s) time period to advance by
    * @return uint64_t
    */
-  [[nodiscard]] uint64_t calc_num_steps(T adv_t) const;
+  [[nodiscard]] uint64_t calc_num_steps(fpp adv_t) const;
 
   /*!
    * advances internal field state by one time step
    * @param dt (s) time step
    * @return void
    */
-  void step(T dt);
+  void step(fpp dt);
 
   /*!
    * returns the number of total voxels per field
@@ -56,21 +56,21 @@ private:
    * FDTDEngine constructor
    * @param config configuration object
    */
-  explicit FDTDEngine(const Config<T> &config);
+  explicit FDTDEngine(const Config &config);
 
   /*!
    * calculates the number of steps required to model a given time span
    * @param time_span (s) time span to be modeled
    * @return number of steps required by CFL stability condition
    */
-  [[nodiscard]] uint64_t calc_cfl_steps(T time_span) const;
+  [[nodiscard]] uint64_t calc_cfl_steps(fpp time_span) const;
 
   /*!
    * advances internal electric field state by one time step
    * @param ea electric field a loop constant
    * @param eb electric field b loop constant
    */
-  void update_e(T ea, T eb);
+  void update_e(fpp ea, fpp eb) const;
 
   /*!
    * advances internal magnetic field state by one time step
@@ -78,84 +78,84 @@ private:
    * @param hya magnetic field a loop constant for y-component
    * @param hza magnetic field a loop constant for z-component
    */
-  void update_h(T hxa, T hya, T hza);
+  void update_h(fpp hxa, fpp hya, fpp hza) const;
 
   /*!
    * advances internal electric field x-component state by one time step
    * @param ea electric field a loop constant
    * @param eb electric field b loop constant
    */
-  void update_ex(T ea, T eb);
+  void update_ex(fpp ea, fpp eb) const;
   /*!
    * advances internal electric field y-component state by one time step
    * @param ea electric field a loop constant
    * @param eb electric field b loop constant
    */
-  void update_ey(T ea, T eb);
+  void update_ey(fpp ea, fpp eb) const;
 
   /*!
    * advances internal electric field z-component state by one time step
    * @param ea electric field a loop constant
    * @param eb electric field b loop constant
    */
-  void update_ez(T ea, T eb);
+  void update_ez(fpp ea, fpp eb) const;
 
   /*!
    * advances internal magnetic field x-component state by one time step
    * @param hya magnetic field a loop constant for y-component
    * @param hza magnetic field a loop constant for z-component
    */
-  void update_hx(T hya, T hza);
+  void update_hx(fpp hya, fpp hza) const;
 
   /*!
    * advances internal magnetic field y-component state by one time step
    * @param hxa magnetic field a loop constant for x-component
    * @param hza magnetic field a loop constant for z-component
    */
-  void update_hy(T hxa, T hza);
+  void update_hy(fpp hxa, fpp hza) const;
 
   /*!
    * advances internal magnetic field z-component state by one time step
    * @param hxa magnetic field a loop constant for x-component
    * @param hya magnetic field a loop constant for y-component
    */
-  void update_hz(T hxa, T hya);
+  void update_hz(fpp hxa, fpp hya) const;
 
   /// (m) size of bounding box in all directions
-  const Coord3<T> len;
+  const Coord3<fpp> len;
 
   /// relative diagonally isotropic permittivity of material inside bounding box
-  const T ep_r;
+  const fpp ep_r;
 
   /// relative diagonally isotropic permeability of material inside bounding box
-  const T mu_r;
+  const fpp mu_r;
 
   /// (F/m) diagonally isotropic permittivity of material inside bounding box
-  const T ep;
+  const fpp ep;
 
   /// (H/m) diagonally isotropic permeability of material inside bounding box
-  const T mu;
+  const fpp mu;
 
   /// (S/m) diagonally isotropic conductivity of material inside bounding box
-  const T sigma;
+  const fpp sigma;
 
   /// number of voxels in all directions
   Coord3<size_t> nv{};
 
   /// (m) spatial increments in all directions
-  Coord3<T> d;
+  Coord3<fpp> d;
 
   /// (m) inverse spatial increments in all directions
-  Coord3<T> d_inv;
+  Coord3<fpp> d_inv;
 
   /// (V/m) electric field vector
-  Vector3<T> e;
+  Vector3<fpp> e;
 
   /// (A/m) magnetic field vector
-  Vector3<T> h;
+  Vector3<fpp> h;
 
   /// (s) elapsed time
-  T time = 0.0;
+  fpp time = 0.0;
 };
 
 #endif // CORE_FDTD_ENGINE_H
