5459312048a08cdeab244ff8a20787d5fdce92b0
sam <sjwyss@outlook.com>
2025-07-09 21:59:37 -0400

    corrected comment style

--- src/constants/type.h
+++ src/constants/type.h
@@ -1,7 +1,7 @@
 #ifndef CONSTANTS_TYPE_H
 #define CONSTANTS_TYPE_H
 
-// floating point precision (e.g., double or float)
+/// floating point precision (e.g., double or float)
 using fpp = float;
 
 #endif // CONSTANTS_TYPE_H
