90fde264c13f0da8246dfa7b3ed5798d37ebfce7
sam <sjwyss@outlook.com>
2025-07-17 16:48:10 -0400

    removed values in header and reordered

--- src/core/config.h
+++ src/core/config.h
@@ -12,38 +12,41 @@
 #include "hdf5_wrapper.h"
 
 struct Config {
-  /// (Hz) maximum frequency to resolve with FDTD engine
-  fpp max_frequency = 15e9;
-
-  /// number of voxels per minimum wavelength for FDTD engine
-  size_t num_vox_min_wavelength = 20;
-
-  /// number of voxels per minimum feature dimension for FDTD engine
-  size_t num_vox_min_feature = 4;
-
-  /// data output downsampling ratio, number of steps between logged timesteps
-  uint64_t ds_ratio = 100000;
-
   /// (s) end time of simulation
-  fpp end_time = 5e-9;
+  fpp end_time;
 
   /// (m) length of bounding box in the x-direction
-  fpp x_len = 0.001;
+  fpp x_len;
 
   /// (m) length of bounding box in the y-direction
-  fpp y_len = 0.001;
+  fpp y_len;
 
   /// (m) length of bounding box in the z-direction
-  fpp z_len = 0.001;
+  fpp z_len;
+
+  /// (Hz) maximum frequency to resolve with FDTD engine
+  fpp max_frequency;
+
+  /// number of voxels per minimum wavelength for FDTD engine
+  size_t num_vox_min_wavelength;
+
+  /// number of voxels per minimum feature dimension for FDTD engine
+  size_t num_vox_min_featur;
 
   /// diagonally isotropic relative permittivity inside bounding box
-  fpp ep_r = 1.0;
+  fpp ep_r;
 
   /// diagonally isotropic relative permeability inside bounding box
-  fpp mu_r = 1.0;
+  fpp mu_r;
 
   /// (S / m) diagonally isotropic conductivity of material in bounding box
-  fpp sigma = 0.0;
+  fpp sigma;
+
+  /// output directory
+  std::filesystem::path out;
+
+  /// data output downsampling ratio, number of steps between logged timesteps
+  uint64_t ds_ratio;
 
   /// output HDF5 file
   HDF5Obj h5;
