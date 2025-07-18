3fea152d3793c99e238a5b067bca45f963ae57f9
sam <sjwyss@outlook.com>
2025-07-18 08:11:08 -0400

    added checks for input file path

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -1,11 +1,34 @@
 #include "config.h"
 
 Config::Config(const std::string &input_file_path) {
-  // todo input file validation
+  SPDLOG_TRACE("enter Config::Config");
+
+  const std::filesystem::path input_file = input_file_path;
+  if (!std::filesystem::exists(input_file)) {
+    SPDLOG_CRITICAL("input file path `{}` not found", input_file_path);
+    throw std::runtime_error(
+        fmt::format("input file path `{}` not found ... please correct and rerun", input_file.string()));
+  }
+  if (!std::filesystem::is_regular_file(input_file)) {
+    SPDLOG_CRITICAL("input file path `{}` is not a regular file", input_file_path);
+    if (!std::filesystem::is_directory(input_file)) {
+      SPDLOG_CRITICAL("input file path `{}` is a directory not a file", input_file_path);
+      throw std::runtime_error(fmt::format(
+          "input file path `{}` is a directory not a file ... please correct and rerun", input_file.string()));
+    }
+    throw std::runtime_error(
+        fmt::format("input file path `{}` is not regular file ... please correct and rerun", input_file.string()));
+  }
+  SPDLOG_DEBUG("input file path `{}` successfully verified", input_file.string());
+
+
+
   parse_time();
   parse_geometry();
   parse_material();
   parse_data();
+
+  SPDLOG_TRACE("exit Config::Config");
 }
 
 std::expected<Config, std::string> Config::create(const std::string &input_file_path) {
