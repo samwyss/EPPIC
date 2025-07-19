874a8ee1e6e80e3340a1c1b297c040c8019d290c
sam <sjwyss@outlook.com>
2025-07-19 15:43:00 -0400

    added single eppic source/header and removed redundant files from executable

---
+++ src/core/eppic.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sam on 7/19/25.
+//
+
+#include "eppic.h"
---
+++ src/core/eppic.h
@@ -0,0 +1,16 @@
+//
+// Created by sam on 7/19/25.
+//
+
+#ifndef EPPIC_H
+#define EPPIC_H
+
+
+
+class eppic {
+
+};
+
+
+
+#endif //EPPIC_H
