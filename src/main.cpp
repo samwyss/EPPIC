6f70c337f5534b93559cd65844bc7cef293a63d1
sam <sjwyss@outlook.com>
2025-07-19 11:34:20 -0400

    revised logging in main

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "type.h"
 #include "world.h"
 
+#include <sys/stat.h>
+
 /*!
  * main driver function and build target
  * @param argc argument count
@@ -16,85 +18,78 @@
 int main(const int argc, char **argv) {
 
   const auto start_time = std::chrono::high_resolution_clock::now();
-  const auto start_time_str = fmt::format("{:%Y-%m-%d_%H:%M:%S}", start_time);
+  const auto id = fmt::format("{:%Y-%m-%d_%H:%M:%S}", start_time);
 
-#if SPDLOG_ACTIVE_LEVEL < SPDLOG_LEVEL_OFF
-  const auto console = spdlog::stdout_color_mt("stdout");
-  const auto err_logger = spdlog::stderr_color_mt("stderr");
-  console->info("EPPIC run begin");
-#endif
-
-  // io prefix validation
   if (argc < 2) {
-#if SPDLOG_ACTIVE_LEVEL < SPDLOG_LEVEL_OFF
-    err_logger->critical("io prefix not provided ... please rerun as `EPPIC <io_prefix>`");
-#endif
+    SPDLOG_CRITICAL("io prefix not provided ... please rerun as `EPPIC <io_prefix>`");
     return EXIT_FAILURE;
   }
 
-  // timestamped io_dir
-  std::filesystem::path io_dir;
-  // directory and logging setup
-  try {
-    const auto io_prefix = std::filesystem::canonical(argv[1]);
-
-    const auto root_dir = io_prefix / "out";
-    if (!is_directory(root_dir)) {
-      create_directory(root_dir);
-    }
-
-    io_dir = root_dir / start_time_str;
-    if (!is_directory(io_dir)) {
-      create_directory(io_dir);
-    }
-
 #if SPDLOG_ACTIVE_LEVEL < SPDLOG_LEVEL_OFF
-    const auto log_dir = io_dir / "logs";
-    if (!is_directory(log_dir)) {
-      create_directory(log_dir);
+  const auto tmp_log_dir = std::filesystem::canonical("./log");
+
+  try {
+    if (!is_directory(tmp_log_dir)) {
+      create_directory(tmp_log_dir);
+    } else {
+      SPDLOG_WARN("found previous temporary logging directory at `{}` ... removing now", tmp_log_dir.string());
+      std::filesystem::remove_all(tmp_log_dir);
+      std::filesystem::create_directory(tmp_log_dir);
     }
+  } catch (const std::filesystem::filesystem_error &err) {
+    SPDLOG_CRITICAL("unable to create temporary logging directory at `{}`: {}", tmp_log_dir.string(), err.what());
+  }
+  SPDLOG_DEBUG("created temporary logging directory `{}`", tmp_log_dir.string());
 
-    const auto logger = spdlog::basic_logger_mt("logger", (log_dir / "log.log").string());
+  const auto tmp_logger = spdlog::basic_logger_mt("logger", (tmp_log_dir / "log.log").string());
+  spdlog::set_default_logger(tmp_logger);
+  spdlog::set_level(spdlog::level::trace); // needed even for compile time logs
+#endif
 
+  SPDLOG_INFO("EPPIC run begin: {}", start_time);
+
+  auto config_result = Config::create(argv[1], id);
+  if (!config_result.has_value()) {
+    SPDLOG_CRITICAL("failed to configure EPPIC: {}", config_result.error());
+    return EXIT_FAILURE;
+  }
+  auto config = std::move(config_result.value());
+
+  try {
+    std::filesystem::rename(tmp_log_dir, config.out / "log");
+    SPDLOG_DEBUG("moved {} to {}", tmp_log_dir.string(), (config.out / "log").string());
+
+    const auto logger = spdlog::basic_logger_mt("logger", (config.out / "logs/log.log").string(), true);
     spdlog::set_default_logger(logger);
     spdlog::set_level(spdlog::level::trace); // needed even for compile time logs
     spdlog::flush_every(std::chrono::seconds(5));
-
-    console->info("file based logger successfully initialized ... remaining logs "
-                  "will be written to {}log.log",
-                  log_dir.string());
-#endif
-
-  } catch (const std::exception &err) {
-#if SPDLOG_ACTIVE_LEVEL < SPDLOG_LEVEL_OFF
-    err_logger->critical("could not setup directory structure and logger: {}", err.what());
-#endif
-
+    SPDLOG_DEBUG("reset default logger to write to `{}`", (config.out / "logs/log.log").string());
+  } catch (const std::filesystem::filesystem_error &err) {
+    SPDLOG_CRITICAL("unable to move `{}` directory to `{}`: {}", tmp_log_dir.string(), (config.out / "log").string(),
+                    err.what());
     return EXIT_FAILURE;
   }
-
-  // todo this will need to be error handled once config is implemented
-  auto config = Config();
-  config.h5 = HDF5Obj(H5Fcreate((io_dir / "data.h5").c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT),
-                      H5Fclose); // todo move to config and create XDMF object elsewhere
 
   auto world_creation_result = World::create(std::move(config));
   if (!world_creation_result.has_value()) {
     SPDLOG_CRITICAL("failed to configure World object: {}", world_creation_result.error());
-
     return EXIT_FAILURE;
   }
-
   auto world = std::move(world_creation_result).value();
 
-  SPDLOG_INFO("configuration successful");
+  const auto config_time = std::chrono::high_resolution_clock::now();
+  SPDLOG_INFO("EPPIC successfully configured: {}", config_time);
+  SPDLOG_INFO("elapsed time (s): {}", config_time - start_time);
 
   if (const auto result = world.advance_to(config.end_time); !result.has_value()) {
-    SPDLOG_CRITICAL("failed to run EPPIC: {}", result.error());
-
+    SPDLOG_CRITICAL("EPPIC run failed: {}", result.error());
     return EXIT_FAILURE;
   }
 
-  SPDLOG_INFO("EPPIC exiting");
+  const auto run_time = std::chrono::high_resolution_clock::now();
+  SPDLOG_INFO("EPPIC run successfully completed: {}", run_time);
+  SPDLOG_INFO("elapsed time (s): {}", run_time - config_time);
+  SPDLOG_INFO("total time (s): {}", run_time - start_time);
+
   return EXIT_SUCCESS;
 }
