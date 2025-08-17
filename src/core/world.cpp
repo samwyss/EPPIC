665da42dfa6ebeff860e35372efd28206e2f204b
sam <sjwyss@outlook.com>
2025-08-17 13:40:10 -0400

    added data_group

--- src/core/world.cpp
+++ src/core/world.cpp
@@ -134,6 +134,8 @@ std::expected<void, std::string> World::advance_by(const fpp adv_t) {
   const auto metadata_group = HDF5Obj(H5Gcreate(h5.get(), "metadata", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT), H5Gclose);
   write_metadata(metadata_group, dt, logged_steps);
 
+  const auto data_group = HDF5Obj(H5Gcreate(h5.get(), "data", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT), H5Gclose);
+
   // loop start time
   // NOTE only used if SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_INFO
   [[maybe_unused]] const auto start_time = std::chrono::high_resolution_clock::now();
