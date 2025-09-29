5f18ba63ed9a194bde99cd6b9e9ac0fe2c5a191f
sam <sjwyss@outlook.com>
2025-09-29 07:10:23 -0400

    added copyright notices

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
+/*
+ * Copyright (C) 2025 Samuel Wyss
+ *
+ * This file is part of EPPIC.
+ *
+ * EPPIC is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * EPPIC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with EPPIC. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 #include <chrono>
 #include <filesystem>
-#include <memory>
 #include <fmt/chrono.h>
+#include <memory>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/spdlog.h>
 
