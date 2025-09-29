e7c943426e5719faa9c6ced481e273739226cad7
sam <sjwyss@outlook.com>
2025-09-29 12:06:31 -0400

    added hdf5 types

--- src/core/type.h
+++ src/core/type.h
@@ -18,6 +18,9 @@
 #ifndef CORE_TYPE_H
 #define CORE_TYPE_H
 
+#include <H5Tpublic.h>
+#include <type_traits>
+
 /// floating point type (e.g., double or float)
 #if EPPIC_USE_FLOAT
 using fp_t = float;
@@ -35,8 +38,25 @@ using ui_t = uint32_t;
 using ui_t = uint64_t;
 #endif
 
+/// HDF5 floating point type
+template <typename T> hid_t h5_fp_t();
+
 // ensure ui_t is either uint64_t or uint32_t to use correct HDF5 and MPI type aliases
-static_assert(std::is_same_v<ui_t, uint64_t> ||
-              std::is_same_v<ui_t, uint32_t>); /// floating point type (e.g., double or float)
+static_assert(std::is_same_v<ui_t, uint64_t> || std::is_same_v<ui_t, uint32_t>);
+
+/// HDF5 floating point type template specialization for double
+template <> inline hid_t h5_fp_t<double>() { return H5T_NATIVE_DOUBLE; }
+
+/// HDF5 floating point type template specialization for float
+template <> inline hid_t h5_fp_t<float>() { return H5T_NATIVE_FLOAT; }
+
+/// HDF5 unsigned integer type
+template <typename T> hid_t h5_ui_t();
+
+/// HDF5 unsigned integer type template specialization for uint64_t
+template <> inline hid_t h5_ui_t<uint64_t>() { return H5T_NATIVE_UINT64; }
+
+/// HDF5 unsigned integer type template specialization for uint32_t
+template <> inline hid_t h5_ui_t<uint32_t>() { return H5T_NATIVE_UINT32; }
 
 #endif // CORE_TYPE_H
