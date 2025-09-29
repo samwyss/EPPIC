cdfe63e720f83e154e3216ab1f008e367524b63e
sam <sjwyss@outlook.com>
2025-09-29 07:06:46 -0400

    removed code for gen xdmf

--- scripts/setup_venv.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# create python virtual environment
-python3 -m venv venv
-source ./venv/bin/activate
-
-# install standard python packages
-pip3 install -r ./scripts/requirements.txt
